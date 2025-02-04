47129bef976af008f2d080354f1a17e386d25060
Chris C <chris.cumming@saturdaymp.com>
2025-02-03 22:15:31 -0700

    Add support for "Conjured Mana Cake" to the item list.
    This commit introduces "Conjured Mana Cake" with initial values to the inventory and updates the test suite to reflect its behavior over 30 days. The changes ensure proper tracking of sell_in and quality attributes for the new item type.

--- src/repo.py
+++ src/repo.py
@@ -11,5 +11,6 @@ def get_items():
          Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
          Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
          # New Item
-         # Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+         Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+         Item(name="Conjured Mana Cake", sell_in=5, quality=20),  # <-- :O
         ]
